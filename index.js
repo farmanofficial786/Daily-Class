1912eab71aa2a85a3485bc129cd9ade15ef9f329
farman ansari <farmanansari35728@gmail.com>
2024-11-05 09:34:20 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,62 +1,100 @@
-// console.log("hello world in javascript");
-
-// .................................................................
-// let a = 5;
-// let b = 10;
-// let res = a+b;
-// console.log(res);
-
-// .................................................................
-// comparison ope.
-// ==
-// let a = 5;
-// let b = 10;
-// let res = a==b;
-// console.log(res);
-
-// .................................................................
-// ===
-// let a = 5;
-// let b = 10;
-// let res = a===b;
-// console.log(res);
+//    JAVASCRIPT  DAILY-CLASS
 
 
-// ................................................................
-// >
-// let a = "9";
-// let b = 10;
-// let res = a<b;
+// increment and decrement opener.....................................................
+
+// let num = 20;
+// console.log(--num,num--,num,num--,num--,num--,--num,num,num--)         // step 1:
+// console.log(num++,num,num++,num++,num++);                       // step 2:
+
+
+// Logical operator ...................................................................
+// &&,!,||
+
+// &&
+// let a = 3;
+// let b = 2;
+// let c = 1;
+// let d = 5;
+// let res =a>b && c<b;
 // console.log(res);
 
 
-//................................................................. 
-// // <
-// let a = "90";
-// let b = 10;
-// let res = a>b;
+// || ..................................................................................
+// let a = 3;
+// let b = 2;
+// let c = 1;
+// let d = 5;
+// let res =a>b || d<b;
 // console.log(res);
 
-// ................................................................
-// let a = "90";
-// let b = 10;
-// b -= 5;
-// console.log(b);
 
-// ...............................................................
-// let a = "90";
-// let b = 10;
-// b *= 5;
-// console.log(b);
+// !....................................................................................
+// let a = 3;
+// let b = 2;
+// let c = 5;
+// let d = 5;
+// let res = a!=b || c!=d;
+// console.log(res);
 
-// ................................................................
-// let a = "90";
-// let b = 10;
-// b /= 5;
-// console.log(b);
 
-// ...............................................................
-// let a = "90";
-// let b = 10;
-// b -= 5;
-// console.log(b);
+// Conditional Operator ................................................................
+
+// let a  = 5;
+// let b  = 5;
+// let res = (a===b ? 'c' : 'g')
+
+// console.log(res);
+
+
+// ....................................................................................
+// let a=4567;
+//  let res = a/1000;
+// console.log(Math.floor(res));
+
+
+// Decision making .....................................................................
+// if else
+
+// let x = 5;
+// if(x>10){
+//     console.log('x is greater than 10');
+// }else {
+//     console.log('x is less then or equal to 10');
+// }
+
+
+
+//Sswitch statement .............................................................
+
+//  let check = 4;
+//  switch (check)
+
+//      case 1:  
+//         console.log("sunday");
+//         break;
+
+//      case 2:
+//         console.log("monday");
+//         break;
+
+//      case 3:
+//         console.log("tuesday");
+//         break;
+
+//      case 4:
+//         console.log("wednesday");
+//         break;
+
+//      case 5:
+//         console.log("thusday");
+//         break;
+
+//      case 6:
+//         console.log("friday");
+//         break;
+
+//      case 7:
+//         console.log("sutarday");
+//         break;
+
