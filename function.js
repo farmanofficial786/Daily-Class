b6808f44917d316058835f4e060b93f9c43a5d0c
farman ansari <farmanansari35728@gmail.com>
2024-11-12 08:56:10 -0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,42 @@
+         // DAILY  CLASS
+
+
+// FUNCTION ......................
+
+
+// In JS, there are 3 ways to create a function:
+
+// 1. Regular Function
+// 2. Arrow Function
+// 3. Function Experesion..
+
+
+
+// Regular function.....................................................................
+
+// function a(){                  // Step 1.
+//      console.log("a");
+// }
+// a();          // calling function..
+
+
+// function a(b){           // Step 2.
+//     return b;
+// }
+// console.log(a(5));
+
+
+
+// Arrow function........................................................................
+
+// const a = (b) => {
+//    return `${b}`;
+// }
+// console.log(a(5));
+
+
+
+// const c = () => {
+//     console.log("hello Aryan");
+// }
+// c();
