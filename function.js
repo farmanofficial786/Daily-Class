6a7356db478830134621279212912734903d99d4
farman ansari <farmanansari35728@gmail.com>
2024-11-13 08:01:38 -0800

    Add files via upload

--- function.js
+++ function.js
@@ -1,42 +1,22 @@
-         // DAILY  CLASS
+// FUNCTION .............................................
 
+// function statement(fname, lname, age){
+//     return `${fname} ${lname} is age ${age}`;            // value return is function..
 
-// FUNCTION ......................
-
-
-// In JS, there are 3 ways to create a function:
-
-// 1. Regular Function
-// 2. Arrow Function
-// 3. Function Experesion..
-
-
-
-// Regular function.....................................................................
-
-// function a(){                  // Step 1.
-//      console.log("a");
 // }
-// a();          // calling function..
+// let fname = "farman";
+// let lname = "ansari";                      // first way..
+// let age = 23;
+// const res = statement(fname, lname, age);
+// console.log(res);
+// // console.log(statement("farman", "ansari", 23));            // second way..
 
 
-// function a(b){           // Step 2.
-//     return b;
+
+// Que:- You are provided an array of integer you have to print the sum of arry..............
+
+// let arr = [1, 2, 3, 4];
+// function sum(a, b, c, d){
+//     return a+b+c+d;
 // }
-// console.log(a(5));
-
-
-
-// Arrow function........................................................................
-
-// const a = (b) => {
-//    return `${b}`;
-// }
-// console.log(a(5));
-
-
-
-// const c = () => {
-//     console.log("hello Aryan");
-// }
-// c();
+// console.log(sum(1, 2, 3, 4));
