c2502df4651d4a4861d4b07321cff63a3da25020
farman ansari <farmanansari35728@gmail.com>
2024-11-14 08:54:00 -0800

    Add files via upload

---
+++ object.js
@@ -0,0 +1,34 @@
+               // DAILY  CLASS
+
+
+
+//OBJECT..................................
+
+// var user = {
+//     fname : "farman",
+//     lname : "ansari",
+//     age : 22
+// }
+// user.fname.age       // first way
+// // let res = user.fname      // second way 
+// console.log(res);
+// console.log(typeof user.fname);    
+
+                                                                                                                                                    
+
+
+// SPREAD OPERATOR IN JS...........................................
+
+// const body ={
+//     height : 5.5,
+//     weigh: 6
+// }
+// const brain = {
+//     iq: 1,
+// }
+// const person = {
+//     ...body,
+//     ...brain,
+//     name: "farman"
+// }
+// console.log(person);
