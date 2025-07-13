b2056258bfbf2eccf58995f2a43822b91162a6ca
farman ansari <farmanansari35728@gmail.com>
2025-07-13 23:57:02 +0530

    Add files via upload

---
+++ Dom.js
@@ -0,0 +1,18 @@
+
+function change(){
+   let creat = document.getElementsByTagName('p')
+   creat[0].innerHTML = "Farman ansari";
+   creat[1].innerHTML = "Sharik ansari";
+   creat[0].style.backgroundColor = "yellow";
+   creat[1].style.backgroundColor = "red";
+};
+
+function fun(){
+    let tagname = document.getElementById('Tag');
+    tagname.innerHTML = "farman ansari!";
+    
+}
+
+let worked = document.getElementById('work');
+worked.innerHTML = "This is a JavaScript";
+worked.style.backgroundColor = "yellow";
