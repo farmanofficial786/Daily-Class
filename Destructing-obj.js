fa39d5f06b9ce570c8817752789192e50e418fc8
farman ansari <farmanansari35728@gmail.com>
2025-07-18 19:08:10 +0530

    Add files via upload

---
+++ Destructing-obj.js
@@ -0,0 +1,13 @@
+// let user = {
+//     name : "farman Ansari",
+//     age : 23,
+//     city : "delhi"
+// }
+
+// let {name : n, age : a, city : c} = user;           // Firest way.......
+
+// // let {name, age, city} = user;               // second way........
+
+// console.log(n);
+// console.log(a);
+// console.log(c);
