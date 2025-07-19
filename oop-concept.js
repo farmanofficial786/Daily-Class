fcf49f986d46c6864256054a8690ccfae1be3f85
farman ansari <farmanansari35728@gmail.com>
2025-07-19 16:29:21 +0530

    Add files via upload

---
+++ oop-concept.js
@@ -0,0 +1,60 @@
+
+// PROTOTYPE...........................................................................
+// class student{
+//     message(){
+//         console.log("Hello Everyone");
+//     };
+
+//     sorry(){
+//         console.log("My name is farman Ansari");
+//     };
+// };
+
+// let a =new student()
+// a.message();
+// a.sorry();
+
+
+
+// class student{
+    
+//     constructor(){
+//         let name;
+//         console.log(constructor);
+//     }
+//     hello(){
+//         console.log(this.name);
+//     }
+// }
+// let a = new student();
+// a.name = "farman ansari";
+// a.hello()
+
+
+// There are three type of class.......................................................
+
+// 1) constructure.
+// 2) prototype.
+// 3) static.
+
+// class student{                                       // CONSTRUCTOR...............
+//     constructor(name, age){
+//         this.studentname = name
+//         this.studentage = age
+//     };
+
+//     hello(){
+//         document.write(`Hello! ${this.studentname} your age is ${this.studentage}`);
+//         // console.log(`Hello ${this.studentname} your age is ${this.studentage}`);
+//     };
+
+//     static staticfunction(){                                                          // STATIC.............
+//         document.write("static function");
+//         // console.log("static function");
+//     };
+
+// };
+// let a = new student("farman Ansari", 23);
+
+// a.hello();
+// student.staticfunction()
