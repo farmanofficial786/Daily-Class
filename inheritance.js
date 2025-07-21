11d060ed05f5ab498a476a2d8a474f69417f0b06
farman ansari <farmanansari35728@gmail.com>
2025-07-21 23:04:24 +0530

    Add files via upload

---
+++ inheritance.js
@@ -0,0 +1,34 @@
+// class employee{                                           // Firest class...........
+//     constructor(name, age, salary){
+//         this.employeename = name
+//         this.employeeage = age
+//         this.employeesalary = salary
+//         // console.log("Constructor Methods");
+//     }
+//     info(){
+//         console.log(`Employee :
+//            name : ${this.employeename}
+//            age : ${this.employeeage}
+//            salary : ${this.employeesalary}
+//         `);
+//     }
+// }
+
+// class manager extends employee {                           //Second class............
+
+// info(){
+//     let to = 1000;
+//     let per = 300;
+//     let totalsalary = this.employeesalary + to + per;
+//     console.log(`Manager :
+//        name : ${this.employeename}
+//        age : ${this.employeeage}
+//        salary : ${totalsalary}
+//     `)};
+// }
+
+
+// let a = new manager("farman Ansari", 23, 50000);
+// let b = new employee("sharik Ansari", 24, 25000);
+// a.info();
+// b.info()
