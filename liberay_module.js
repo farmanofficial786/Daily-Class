459d264775fec9c7c2664fc538c416239be395d3
farman ansari <farmanansari35728@gmail.com>
2025-08-12 16:17:10 +0530

    Add files via upload

---
+++ liberay_module.js
@@ -0,0 +1,31 @@
+// FIREST WebAssembly.................................
+// export let name = "Farman Ansari";
+
+// export function functionname(){
+//     // console.log("Hello Everyone");
+//     return `Hello ${functionname}`
+// }
+
+// export class user{
+//     constructor(){
+//         console.log("constructor methods");
+//     }
+// }
+
+
+
+// SECOND WAY...................................
+let name = "Farman Ansari";
+
+function functionname(){
+    // console.log("Hello Everyone");
+    return `Hello ${functionname}`
+}
+
+class user{
+    constructor(){
+        console.log("constructor methods");
+    }
+}
+
+// export {name, functionname, user}
---
+++ main-module.js
@@ -0,0 +1,11 @@
+// import { name, functionname, user // user as us } from "./liberay_module";      //firest way..........
+// import * as from "./liberay_module";                                          // second way..........
+
+// console.log(name);
+// console.log(functionname());
+// let a = new user();
+
+
+// console.log(target.name);
+// console.log(target.functionname());
+// let a = new target.user();
